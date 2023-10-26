0e82116401a7366e229c3bf5ed87eeb8d32ee62d
Esteban Desmedt <estebandesmedt33@gmail.com>
2023-10-26 17:59:01 +0200

    tetris methods

---
+++ Python/tetris.py
@@ -0,0 +1,23 @@
+#Tetris Python
+
+def moveRight():
+    # horizontal index +1
+
+def moveLeft():
+    # horizontal index -1
+
+def moveDown():
+    # vertical index +1
+
+def rotatePiece():
+    # rotate piece
+
+def lineRemove():
+    # remove line and add score with scoreCount()
+
+def scoreCount():
+    # count score
+
+
+
+    
