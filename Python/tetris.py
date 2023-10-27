64e430e150963511ccf2c3d7c9b57481a64f56ce
Esteban Desmedt <estebandesmedt33@gmail.com>
2023-10-27 22:07:18 +0200

    tetris pygame gameloop

--- Python/tetris.py
+++ Python/tetris.py
@@ -1,46 +1,46 @@
 #Tetris Python
 
-# # Tetris welcome
-# print("-------------TETRIS-------------")
-# print("................................")
+# Tetris welcome
+print("-------------TETRIS-------------")
+print("................................")
 
-# # Create a fixed 2D array of size 20x10 with each cell initialized to the string "[ ]"
-# tetrisGrid = [["[ ]" for _ in range(10)] for _ in range(20)]
+# Create a fixed 2D array of size 20x10 with each cell initialized to the string "[ ]"
+tetrisGrid = [["[ ]" for _ in range(10)] for _ in range(20)]
 
-# # Print the 2D array with borders
-# for i, row in enumerate(tetrisGrid):
-#     # Print the left border
-#     print("|", end='')
+# Print the 2D array with borders
+for i, row in enumerate(tetrisGrid):
+    # Print the left border
+    print("|", end='')
 
-#     for j, element in enumerate(row):
-#         print(element, end='')
+    for j, element in enumerate(row):
+        print(element, end='')
 
-#     # Print the right border
-#     print("|")
+    # Print the right border
+    print("|")
 
-# # Print the bottom border with the correct length
-# print("+" + "-" * (3 * len(tetrisGrid[0])) + "+")
-
-
+# Print the bottom border with the correct length
+print("+" + "-" * (3 * len(tetrisGrid[0])) + "+")
 
 
-# def moveRight():
-#     # horizontal index +1
 
-# def moveLeft():
-#     # horizontal index -1
 
-# def moveDown():
-#     # vertical index +1
+def moveRight():
+    # horizontal index +1
 
-# def rotatePiece():
-#     # rotate piece
+def moveLeft():
+    # horizontal index -1
 
-# def lineRemove():
-#     # remove line and add score with scoreCount()
+def moveDown():
+    # vertical index +1
 
-# def scoreCount():
-#     # count score
+def rotatePiece():
+    # rotate piece
+
+def lineRemove():
+    # remove line and add score with scoreCount()
+
+def scoreCount():
+    # count score
 
 
 
