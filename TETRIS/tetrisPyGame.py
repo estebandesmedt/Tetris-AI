72db6b0ddc484ddec0a44e9668457a5247c14f34
Esteban Desmedt <estebandesmedt33@gmail.com>
2023-10-29 22:02:29 +0100

    tetris collisions

--- TETRIS/tetrisPyGame.py
+++ TETRIS/tetrisPyGame.py
@@ -1,6 +1,5 @@
 import pygame, sys
-from grid import Grid
-from blocks import *
+from game import Game
 
 pygame.init()
 dark_blue = (44,44,127)
@@ -10,20 +9,30 @@ pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 
-game_grid = Grid()
+game = Game()
 
-block = TBlock()
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_DOWN:
+                game.move_down()
+            if event.key == pygame.K_UP:
+                game.rotate()
+        if event.type == GAME_UPDATE:
+            game.move_down()
 
     screen.fill(dark_blue)
-    game_grid.draw(screen)
-    block.draw(screen)
-
+    game.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
