2b0a6458956fd49e3d521de33b4a9978ba9bae47
Esteban Desmedt <estebandesmedt33@gmail.com>
2023-10-29 18:48:15 +0100

    tetris blocks

--- TETRIS/tetrisPyGame.py
+++ TETRIS/tetrisPyGame.py
@@ -1,4 +1,6 @@
 import pygame, sys
+from grid import Grid
+from blocks import *
 
 pygame.init()
 dark_blue = (44,44,127)
@@ -8,6 +10,10 @@ pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 
+game_grid = Grid()
+
+block = TBlock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,6 +21,9 @@ while True:
             sys.exit()
 
     screen.fill(dark_blue)
+    game_grid.draw(screen)
+    block.draw(screen)
+
 
     pygame.display.update()
     clock.tick(60)
